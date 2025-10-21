96fb9cd30ee6d8f5e03f7f2b72949a3ec2b08270
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-10-21 10:11:20 +0200

    add sifting delection

---
+++ AlbionImageParser/RegionDetection.cs
@@ -0,0 +1,91 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Features2D;
+
+namespace AlbionImageParser;
+
+public class RegionDetection
+{
+    public static void SiftMatches(Mat scene, Mat template, string outPath)
+    {
+        if (scene.Empty() || template.Empty())
+        {
+            Console.WriteLine("Could not load images.");
+            return;
+        }
+
+        var sift = SIFT.Create();
+        var bf = new BFMatcher(NormTypes.L2, crossCheck: false);
+
+        using var descTemplate = new Mat();
+        using var descScene = new Mat();
+
+        sift.DetectAndCompute(template, null, out var kpTemplate, descTemplate);
+        sift.DetectAndCompute(scene, null, out var kpScene, descScene);
+
+        if (descTemplate.Empty() || descScene.Empty())
+        {
+            Console.WriteLine("No descriptors found.");
+            return;
+        }
+
+        // KNN matching
+        var knnMatches = bf.KnnMatch(descTemplate, descScene, k: 2);
+
+        const double threshold = .5;
+        
+        // Lowe’s ratio test
+        var goodMatches = new List<DMatch>();
+        foreach (var m in knnMatches)
+        {
+            if (m.Length == 2 && m[0].Distance < threshold * m[1].Distance)
+                goodMatches.Add(m[0]);
+        }
+
+        Console.WriteLine($"Good matches: {goodMatches.Count}");
+
+        if (goodMatches.Count >= 4)
+        {
+            // Use Point2f (float) for homography
+            var srcPts = goodMatches.Select(m => kpTemplate[m.QueryIdx].Pt).ToArray();
+            var dstPts = goodMatches.Select(m => kpScene[m.TrainIdx].Pt).ToArray();
+            
+            Mat homography = Cv2.FindHomography(InputArray.Create(srcPts), InputArray.Create(dstPts),
+                                                HomographyMethods.Ransac, 5.0);
+
+            if (!homography.Empty())
+            {
+                Point2f[] templateCorners =
+                {
+                    new(0, 0),
+                    new(template.Cols, 0),
+                    new(template.Cols, template.Rows),
+                    new(0, template.Rows)
+                };
+
+                Point2f[] sceneCorners = Cv2.PerspectiveTransform(templateCorners, homography);
+
+                // Draw box around detected area
+                for (int i = 0; i < 4; i++)
+                {
+                    var pt1 = sceneCorners[i];
+                    var pt2 = sceneCorners[(i + 1) % 4];
+                    Cv2.Line(scene, (int)pt1.X, (int)pt1.Y, (int)pt2.X, (int)pt2.Y, Scalar.White, 3);
+                }
+
+                var e = new Mat();
+                Cv2.Resize(scene, e, new Size(1920, 1080));
+                    
+                Cv2.ImWrite(outPath, e);
+                Cv2.WaitKey();
+            }
+            else
+            {
+                Console.WriteLine("Homography could not be computed.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Not enough good matches found.");
+        }
+    }
+}
--- AlbionImageParser/Test1.cs
+++ AlbionImageParser/Test1.cs
@@ -1,11 +1,67 @@
-﻿namespace AlbionImageParser;
+﻿using OpenCvSharp;
+using Tesseract;
+
+namespace AlbionImageParser;
 
 [TestClass]
 public sealed class Test1
 {
+
+    [TestMethod]
+    public void Region()
+    {
+        var outputPath = Path.GetFullPath("../../../../.output");
+        
+        
+        var assetsPath = Path.Combine(Directory.GetCurrentDirectory(), "Assets");
+        var iconPath = Path.GetFullPath(Path.Combine(assetsPath, @"icon.png"));
+        var imagesPath = Path.GetFullPath(Path.Combine(assetsPath, @"samples"));
+        var images = Directory.GetFiles(imagesPath);
+        
+        // foreach (var image in images)
+        // {
+        //     var newFileName = Path.Combine(outputPath, Path.GetFileName(image));
+        //     Console.WriteLine(newFileName);
+        // }
+
+        foreach (var image in images)
+        {
+            var newPath = Path.Combine(outputPath, Path.GetFileName(image));
+            Console.WriteLine(newPath);
+            File.Delete(newPath);
+            RegionDetection.SiftMatches(Cv2.ImRead(image, ImreadModes.Grayscale),
+                Cv2.ImRead(iconPath, ImreadModes.Grayscale), newPath);
+        }
+        
+        // RegionDetection.SiftMany(Cv2.ImRead(images[0], ImreadModes.Grayscale), Cv2.ImRead(iconPath, ImreadModes.Grayscale));
+    }
+    
     [TestMethod]
     public void TestMethod1()
     {
-        ImageParser.Parse();
+        var assetsPath = Path.Combine(Directory.GetCurrentDirectory(), "Assets");
+        var tessdataPath = Path.GetFullPath(Path.Combine(assetsPath, "tessdata"));
+        var iconPath = Path.GetFullPath(Path.Combine(assetsPath, @"icon.png"));
+        var imagePath = Path.GetFullPath(Path.Combine(assetsPath, @"samples\1.png"));
+        
+        var parser = new ScreenshotParser(tessdataPath, iconPath);
+        var result = parser.Parse(imagePath);
+
+        if (result != null)
+            Console.WriteLine(result);
+        else
+            Console.WriteLine("No portal box detected.");
+    }
+
+    [TestMethod]
+    public void T2()
+    {
+        var assetsPath = Path.Combine(Directory.GetCurrentDirectory(), "Assets");
+        var tessdataPath = Path.GetFullPath(Path.Combine(assetsPath, "tessdata"));
+
+        using (var engine = new TesseractEngine(tessdataPath, "eng", EngineMode.Default))
+        {
+            
+        }
     }
 }
