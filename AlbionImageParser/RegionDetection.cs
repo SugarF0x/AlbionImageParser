1eab959de0d8df4ec0b27a2ebe9fbf17743cc529
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-10-21 19:14:09 +0200

    throw error instead of returning null

--- AlbionImageParser/RegionDetection.cs
+++ AlbionImageParser/RegionDetection.cs
@@ -1,7 +1,6 @@
 ﻿using System.Text.RegularExpressions;
 using OpenCvSharp;
 using OpenCvSharp.Extensions;
-using OpenCvSharp.Features2D;
 using Tesseract;
 using Rect = OpenCvSharp.Rect;
 
@@ -10,19 +9,17 @@ namespace AlbionImageParser;
 public static partial class RegionDetection
 {
     public struct SampleRegionData(string source, string target, string timeout);
-    public static SampleRegionData? Parse(string sampleSrc, string templateSrc)
+    public static SampleRegionData Parse(string sampleSrc, string templateSrc)
     {
         var (sample, template) = PrepareSample(sampleSrc, templateSrc);
         using (sample)
         using (template)
         {
             var regions = TemplateMatch(sample, template);
-        
-            if (regions is not { } r) return null;
             return new SampleRegionData(
-                ExtractText(sample, r.Source),
-                ExtractText(sample, r.Target),
-                ExtractTimeout(sample, r.Timeout)
+                ExtractText(sample, regions.Source),
+                ExtractText(sample, regions.Target),
+                ExtractTimeout(sample, regions.Timeout)
             );            
         }
     }
@@ -40,14 +37,14 @@ public static partial class RegionDetection
     }
 
     private readonly record struct MatchedSampleRegions(Rect Source, Rect Target, Rect Timeout);
-    private static MatchedSampleRegions? TemplateMatch(Mat sample, Mat template)
+    private static MatchedSampleRegions TemplateMatch(Mat sample, Mat template)
     {
         using var result = new Mat();
 
         Cv2.MatchTemplate(sample, template, result, TemplateMatchModes.CCoeffNormed);
         Cv2.MinMaxLoc(result, out _, out var maxVal, out _, out var maxLoc);
 
-        if (maxVal < .9) return null;
+        if (maxVal < .9) throw new InvalidImage("Could not match template image - portal frame missing or obstructed");
         return new MatchedSampleRegions(
             new Rect(350, 40, 660 - 350, 77 - 40),
             new Rect(maxLoc.X - 208, maxLoc.Y - 35, 208 + 35, 35 - 8),
@@ -56,11 +53,11 @@ public static partial class RegionDetection
     }
 
     [System.Diagnostics.CodeAnalysis.SuppressMessage("", "CA1416", Justification = "Not accessible on non windows machines anyway")]
-    private static (string text, float confidence) OCR(Mat sample, Rect rect)
+    private static (string text, float confidence) OcrRead(Mat sample, Rect rect)
     {
         using var crop = CropMat(sample, rect);
         
-        var tessDataPath = Path.Combine(Directory.GetCurrentDirectory(), "Assets", "tessdata");
+        var tessDataPath = Path.Combine(Directory.GetCurrentDirectory(), "Assets", "tessData");
         using var engine = new TesseractEngine(tessDataPath, "eng", EngineMode.Default);
         
         using var pix = PixConverter.ToPix(crop.ToBitmap());
@@ -70,14 +67,14 @@ public static partial class RegionDetection
 
     private static string ExtractText(Mat sample, Rect rect)
     {
-        var (text, confidence) = OCR(sample, rect);
+        var (text, confidence) = OcrRead(sample, rect);
         Console.WriteLine($"[{text}] Confidence: {confidence}");
         return text;
     }
 
     private static string ExtractTimeout(Mat sample, Rect rect)
     {
-        var (text, confidence) = OCR(sample, rect);
+        var (text, confidence) = OcrRead(sample, rect);
         var matches = TimeoutSplitRegex().Matches(text);
 
         var result = "";
@@ -103,3 +100,5 @@ public static partial class RegionDetection
     [GeneratedRegex(@"\D")]
     private static partial Regex UnitRegex();
 }
+
+public class InvalidImage(string message) : Exception(message);
